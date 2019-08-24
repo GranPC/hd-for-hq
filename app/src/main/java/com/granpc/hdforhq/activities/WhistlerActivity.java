3b4d5d6904e0c289cbd7fc704720b513947a8f45
jesus <jesus@itch.io>
2019-08-24 14:23:12 +0200

    Whistler: add sound fx, better start button layout code

--- app/src/main/java/com/granpc/hdforhq/activities/WhistlerActivity.java
+++ app/src/main/java/com/granpc/hdforhq/activities/WhistlerActivity.java
@@ -7,7 +7,13 @@ import android.graphics.Typeface;
 import android.graphics.drawable.RippleDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RoundRectShape;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.view.OnApplyWindowInsetsListener;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.WindowInsetsCompat;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -22,6 +28,7 @@ import android.widget.TextView;
 import com.granpc.hdforhq.models.ApiWhistlerAnswerResult;
 import com.granpc.hdforhq.models.ApiWhistlerGame;
 
+import java.io.File;
 import java.util.Arrays;
 
 import io.reactivex.Flowable;
@@ -32,9 +39,27 @@ import io.reactivex.schedulers.Schedulers;
 public class WhistlerActivity extends BaseHQActivity
 {
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private MediaPlayer bgMusic;
 
     public WhistlerActivity()
     {
+    }
+
+    private MediaPlayer playSound( String name )
+    {
+        File f = new File( Environment.getExternalStorageDirectory().getPath() + "/HD4HQ/res/sfx/" + name + ".mp3" );
+
+        if ( f.exists() )
+        {
+            MediaPlayer player = MediaPlayer.create( thiz, Uri.fromFile( f ) );
+            if ( player != null )
+            {
+                player.start();
+                return player;
+            }
+        }
+
+        return null;
     }
 
     // I really ought to figure out a way to import XML layouts...
@@ -84,20 +109,30 @@ public class WhistlerActivity extends BaseHQActivity
         buttonShape.getPaint().setColor( 0xFFFFD02D );
         RippleDrawable rippleDrawable = new RippleDrawable( ColorStateList.valueOf( 0xFFC4A020 ), buttonShape, buttonShape );
 
-        Button startButton = new Button( thiz );
+        final Button startButton = new Button( thiz );
         startButton.setText( "Start" );
         startButton.setTypeface( hqFont, Typeface.BOLD );
         startButton.setAllCaps( false );
         startButton.setTextColor( 0xff36399a );
         startButton.setBackground( rippleDrawable );
-
-        RelativeLayout.LayoutParams bottom = new RelativeLayout.LayoutParams(
+        final int buttonMargin = (int) TypedValue.applyDimension( TypedValue.COMPLEX_UNIT_DIP, 20, thiz.getResources().getDisplayMetrics() );
+        final RelativeLayout.LayoutParams buttonLayout = new RelativeLayout.LayoutParams(
             (int) (scrW / 2.33f), RelativeLayout.LayoutParams.WRAP_CONTENT );
-        bottom.setMargins( 0, 0, 0, 180 );
-        bottom.addRule( RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE );
-        bottom.addRule( RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE );
+        buttonLayout.addRule( RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE );
+        buttonLayout.addRule( RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE );
 
-        layout.addView( startButton, bottom );
+        ViewCompat.setOnApplyWindowInsetsListener( thiz.getWindow().getDecorView(), new OnApplyWindowInsetsListener()
+        {
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets( View v, WindowInsetsCompat insets )
+            {
+                buttonLayout.setMargins( 0, 0, 0, insets.getStableInsetBottom() + buttonMargin );
+                startButton.setLayoutParams( buttonLayout );
+                return insets.consumeSystemWindowInsets();
+            }
+        } );
+
+        layout.addView( startButton, buttonLayout );
         return layout;
     }
 
@@ -116,6 +151,11 @@ public class WhistlerActivity extends BaseHQActivity
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
         thiz.getWindow().setStatusBarColor( Color.TRANSPARENT );
         thiz.setContentView( generateLayout() );
+
+        playSound( "whistlerSplash1.0" );
+        bgMusic = playSound( "whistlerBed3.1" );
+        if ( bgMusic != null )
+            bgMusic.setLooping( true );
 
         Log.d( "HD4HQ", "whistler starting game" );
         Flowable<ApiWhistlerGame> game = getAuthedApi().whistlerStartGame();
