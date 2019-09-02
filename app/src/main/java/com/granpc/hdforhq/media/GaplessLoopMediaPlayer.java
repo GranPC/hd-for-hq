0b704ca7bd24595ce91ca4ef7c31b022a18dd50f
jesus <jesus@itch.io>
2019-09-02 14:19:09 +0200

    GaplessLoopMediaPlayer: pausing and seeking the next MediaPlayer to the beginning isn't enough for setNextMediaPlayer - reload it entirely. Maybe fixes #1.

--- app/src/main/java/com/granpc/hdforhq/media/GaplessLoopMediaPlayer.java
+++ app/src/main/java/com/granpc/hdforhq/media/GaplessLoopMediaPlayer.java
@@ -8,14 +8,16 @@ import android.util.Log;
 
 import java.io.File;
 
-public class GaplessLoopMediaPlayer implements MediaPlayer.OnCompletionListener
+public class GaplessLoopMediaPlayer implements MediaPlayer.OnCompletionListener, MediaPlayer.OnPreparedListener
 {
     private MediaPlayer currentPlayer;
     private MediaPlayer nextPlayer;
     private File file;
+    private Context context;
 
     public GaplessLoopMediaPlayer( Context context, String path )
     {
+        this.context = context;
         file = new File( path );
         if ( file.exists() )
         {
@@ -43,13 +45,30 @@ public class GaplessLoopMediaPlayer implements MediaPlayer.OnCompletionListener
     }
 
     @Override
+    public void onPrepared( MediaPlayer mp )
+    {
+        currentPlayer.setNextMediaPlayer( mp );
+        mp.setOnPreparedListener( null );
+    }
+
+    @Override
     public void onCompletion( MediaPlayer mp )
     {
+        Log.d( "HD4HQ", "playback completed" );
         currentPlayer = nextPlayer;
         nextPlayer = mp;
-        nextPlayer.pause();
-        nextPlayer.seekTo( 0 );
-        currentPlayer.setNextMediaPlayer( nextPlayer );
+        nextPlayer.reset();
+        try
+        {
+            nextPlayer.setDataSource( context, Uri.fromFile( file ) );
+            nextPlayer.setOnPreparedListener( this );
+            nextPlayer.prepareAsync();
+        }
+        catch ( Exception e )
+        {
+            Log.d( "HD4HQ", "GaplessLoopMediaPlayer: failed to prepare next, loop will break." );
+            e.printStackTrace();
+        }
     }
 
     public Boolean isValid()
